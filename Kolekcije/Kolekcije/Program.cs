b626b77ee72b415276aa421271ba2823b5a015d4
Raul-Holik <113007938+Raul-Holik@users.noreply.github.com>
2023-02-08 09:30:37 +0100

    Add files via upload

---
+++ Kolekcije/Kolekcije/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolekcije
+{
+    internal class Program
+    {
+        class Klasa
+        {
+            int godina;
+            string prezime;
+
+            public Klasa (int godina, string prezime)
+            {
+                this.godina = godina;
+                this.prezime = prezime;
+            }
+
+            public override string ToString()
+            {
+                string ispis = "Godina: " + godina + " prezime: " + prezime;
+                return ispis;
+            }
+             public int Godina { get => godina; set => godina = value; }
+        }
+        static void Main(string[] args)
+        {
+            ArrayList al = new ArrayList();
+
+            int broj = 1;
+            string ime = "Ime";
+            bool provjera=false;
+            Klasa objekt=new Klasa(2023, "Prezime");
+
+            al.Add(objekt);
+            al.Add(provjera);
+            al.Add(broj);
+            al.Add(ime);
+
+            for(int i = 0; i < al.Count; i++)
+            {
+                Console.WriteLine(al[i]);
+            }
+
+            al.RemoveAt(2);
+            
+            for (int i = 0; i < al.Count; i++)
+            {
+                Console.WriteLine(al[i]);
+            }
+
+            al.Insert(2, broj);
+            for (int i = 0; i < al.Count; i++)
+            {
+                Console.WriteLine(al[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
